6aa1bfda286c94bd1f39ac214e181e7b8bcb33bb
MaxDronCode <Mona Lisa>
2025-03-18 18:37:24 +0100

    Update dependencies and improve SearchInput component

--- formKitFet/components/SearchInput.vue
+++ formKitFet/components/SearchInput.vue
@@ -1,26 +1,41 @@
-<script setup lang="ts">
-defineProps({
-  modelValue: { type: String, required: true },
-});
+<script setup>
+import { ref, watch } from 'vue'
 
-defineEmits<{
-  (e: "update:modelValue", payload: string): void;
-}>();
+const props = defineProps({
+  modelValue: { type: String, required: true }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const inputValue = ref(props.modelValue)
+let timeoutId = null // <-- Simplificamos la declaración
+
+watch(inputValue, (newVal) => {
+  clearTimeout(timeoutId)
+  timeoutId = setTimeout(() => {
+    emit("update:modelValue", newVal)
+  }, 300)
+})
+
+watch(
+  () => props.modelValue,
+  (newVal) => {
+    inputValue.value = newVal
+  }
+)
 </script>
+
 <template>
   <div class="relative">
     <IconSearch
       class="w-3 w-3 absolute top-[50%] translate-y-[-50%] left-2 opacity-30"
     />
-
+    
     <input
       type="text"
       placeholder="Search"
       class="pl-10 p-2 rounded"
-      @input="
-        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-      "
-      :value="modelValue"
+      v-model="inputValue"
     />
   </div>
 </template>
